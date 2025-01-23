c9c61d8e7c50fc1a41d883982eb9940c7162a8ca
Sergey <konsaltpns@gmail.com>
2025-01-23 14:20:31 +0200

    jjjjj

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,122 @@
-alert("hello-1")
+const images = [
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+},
+{
+    preview:
+    'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+    'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+},
+];
+const head = document.querySelector('head');
+const style = document.createElement('style');
+
+style.textContent = `
+.gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 48px 24px;
+    list-style: none;
+    padding: 10px 0;
+    width: 1150px;
+    background-color: aqua;
+    margin: 0 auto;
+}
+.gallery img {
+    max-width: 360px;
+    height: auto;
+    border-radius: 5px;
+    transition: transform 0.3s ease;
+}
+.gallery img:hover {
+    transform: scale(1.1);
+}
+`;
+head.appendChild(style);
+
+// Создаем элемент <ul>
+const body = document.body;
+const ul = document.createElement('ul');
+ul.classList.add('gallery')
+body.insertBefore(ul, body.firstChild);
+
+for (const obj of images) {
+    const liItem = document.createElement('li');
+    const linkA = document.createElement('a');
+    const imgA = document.createElement('img');    
+    liItem.classList.add('gallery-item');
+    linkA.classList.add('gallery-link');
+    imgA.classList.add('gallery-link');
+    linkA.setAttribute('href', `${obj.original}`);
+    imgA.classList.add('gallery-image');
+    imgA.src = `${obj.preview}`;
+    imgA.alt = `${obj.description}`;
+    ul.appendChild(liItem);
+    liItem.appendChild(linkA);
+    linkA.appendChild(imgA);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const lightbox = new SimpleLightbox('.gallery li a', {
+      captions: true, // Додаємо підписи до зображень
+      captionsData: 'alt', // Отримуємо підписи із атрибута alt
+      captionDelay: 250, // Затримка перед з'являнням підпису (мс)
+    });
+
+});
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,56 @@
-alert("Hello 2");
+// Оголошуємо об'єкт formData поза функціями
+const formData = { email: "", message: "" };
+// Обираємо форму та її елементи
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = "feedback-form-state";
+
+// Завантажуємо дані з локального сховища, якщо вони існують
+document.addEventListener('DOMContentLoaded', () =>
+{
+    if (localStorage.getItem("feedback-form-state"))
+        {
+            // Отримуємо дані з localStorage
+            const storedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+            // Заповнюємо форму та об'єкт з сховища даних даних
+            for (const key in storedData)
+            {
+                const input = form.querySelector(`[name="${key}"]`);
+                if (input)
+                    {
+                        input.value = storedData[key]; // Встановлюємо значення для кожного поля
+                        formData[key] = storedData[key]; // Оновлюємо об'єкт formData
+                    }
+            }
+        }
+    else {
+        // Якщо відсутні дані, залишаємо поля порожніми
+        console.log("Дані в локальному сховищі відсутні.");
+        }
+});
+
+// Оновлюємо об'єкт formData та локальне сховище в ході зміни даних у формі
+form.addEventListener("input", function (event) {
+    const target = event.target;
+    if (target.name) {
+        formData[target.name] = target.value.trim();
+        // Сохраняем изменения в localStorage
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    }    
+});
+
+ // Обробка події submit
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Запобігаємо перезавантаженню сторінки
+    // Перевіряємо, чи всі поля заповнені
+    if (!formData.email || !formData.message) {
+        console.log("Fill please all fields");
+        return;
+    }
+    // Виводимо об'єкт formData у консоль
+    console.log("Submitted Data:", formData);
+    // Очищаємо локальне сховище, об'єкт formData і форму
+    localStorage.removeItem(STORAGE_KEY);
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+});
